fbeaa355fd0f7bd5424061749386a99a8be394cc
Aaron Rodi <metalhead5209@gmail.com>
2022-05-25 06:38:33 -0500

    setup auth for user registration route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const asyncWrap = require('../utilities/AsyncWrap');
 const User = require('../models/user');
 
 
@@ -7,8 +8,17 @@ router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', async (req, res) => {
-    res.send(req.body);
-});
+router.post('/register', asyncWrap(async (req, res) => {
+    try {
+    const { email, username, password } = req.body;
+    const user = new User({email, username});
+    const newUser = await User.register(user, password);
+    req.flash('success','Welcome');
+    res.redirect('/festivals');
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}));
 
 module.exports = router;
