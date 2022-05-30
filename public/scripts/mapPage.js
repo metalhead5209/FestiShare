50432dcbeb0adc1b3f2f5561704dc92ba31dbae1
Aaron Rodi <metalhead5209@gmail.com>
2022-05-30 11:08:27 -0500

    added map with geo location of festival to show page

--- public/scripts/mapPage.js
+++ public/scripts/mapPage.js
@@ -1,7 +1,40 @@
-mapboxgl.accessToken = mapToken;
+
+mapboxgl.accessToken = mapBoxToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    style: 'mapbox://styles/mapbox/satellite-streets-v11', // style URL
+    center: festival.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
  });
+
+
+
+
+
+
+const markerHeight = 50;
+const markerRadius = 10;
+const linearOffset = 25;
+const popupOffsets = {
+    'top': [0, 0],
+    'top-left': [0, 0],
+    'top-right': [0, 0],
+    'bottom': [0, -markerHeight],
+    'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+    'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+    'left': [markerRadius, (markerHeight - markerRadius) * -1],
+    'right': [-markerRadius, (markerHeight - markerRadius) * -1]
+};
+
+new mapboxgl.Marker({
+    color: '#ff0000'
+}).setLngLat(festival.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset: popupOffsets, className: 'my-class'})
+        .setHTML(
+            `<h3>${festival.title}</h3><p>${festival.location}</p>`
+        )
+    )
+    .addTo(map);
+
+ 
